fb3ba59e6dffc085e4e562732a7f73337ea3d6f4
cargo <mohamed2003torky@gmail.com>
2024-07-04 14:16:17 +0800

    Fixed a Bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -805,7 +805,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
   <!--[if mso]><style>.v-button {background: transparent !important;}</style><![endif]-->
 <div align="center">
   <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="" style="height:43px; v-text-anchor:middle; width:142px;" arcsize="9.5%"  stroke="f" fillcolor="#b41d1d"><w:anchorlock/><center style="color:#FFFFFF;"><![endif]-->
-    <a href="https://marketifyutm.vercel.app/resetPassword/${user._id} class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #b41d1d; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 19px;">
+    <a href="https://marketifyutm.vercel.app/resetPassword/${user._id}" class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #b41d1d; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 19px;">
       <span style="display:block;padding:10px 20px;line-height:120%;"><span style="line-height: 16.8px;">Reset Password</span></span>
     </a>
     <!--[if mso]></center></v:roundrect><![endif]-->
